97451ef87a982dc8daec0a0ef5e40525b5d8f238
VistaMarston <63620520+VistaMarston@users.noreply.github.com>
2022-04-16 13:23:10 -0600

    Compost Item Class
    Added Methods and implemented methods into the Compost Item Class.

--- EducationalApp/compostitems.cpp
+++ EducationalApp/compostitems.cpp
@@ -1,6 +1,63 @@
 #include "compostitems.h"
 
-CompostItems::CompostItems()
+CompostItems::CompostItems(int itemIndex)
 {
+    setImage(itemIndex);
+    setDescription(itemIndex);
+    setName(itemIndex);
+}
 
+Items::ItemType CompostItems::getType(){
+    return Items::ItemType::Compost;
+}
+
+std::string CompostItems::getName(){
+    return name;
+}
+
+std::string CompostItems::getDescription() {
+    return description;
+}
+
+void CompostItems::setImage(int index){
+    switch(index) {
+    case 0: { // banana Peel
+        QImage image(":/images/images/pinkTulip1.png");
+        itemImage = image.copy();
+        break;
+    } case 1: { // Lawn Clippings
+        QImage image(":/images/images/pinkTulip1.png");
+        itemImage = image.copy();
+        break;
+    }
+    }
+}
+
+void CompostItems::setName(int index) {
+    switch(index) {
+    case 0: {
+        name = "Banana Peel";
+        break;
+    } case 1: {
+        name = "Lawn Clippings";
+        break;
+    }
+    }
+}
+
+void CompostItems::setDescription(int index) {
+    switch(index) {
+    case 0: { // Banana Peel
+        description = "Whole fruits and vegetables and their"
+                      " scraps can go in the compost bin. Just "
+                      "make sure that all of the stickers are removed";
+        break;
+    } case 1: { // Lawn Clippings
+        description = "Most yard work waste such as lawn clippings, "
+                      "leaves, branches, and weeds can go in the "
+                      "compost bin. However, plastic bags, sod, dirt, rocks, construction "
+                      "wood can not go in compost bin";
+        break;
+    }
+    }
 }
--- EducationalApp/compostitems.h
+++ EducationalApp/compostitems.h
@@ -2,6 +2,7 @@
 #define COMPOSTITEMS_H
 
 #include "items.h"
+#include <QImage>
 
 /**
  * @brief The CompostItems class - This class is used to
@@ -12,16 +13,77 @@ class CompostItems : public Items
 {
 public:
     /**
-     * @brief CompostItems - The default constructor
-     * for compost items.
+     * @brief CompostItems - The constructor for compost items
+     * @param index - Indicates which compost item to create.
      */
-    CompostItems();
+    CompostItems(int index);
 
     /**
      * @brief ~CompostItems - The destructor for the
      * compost items.
      * */
     ~CompostItems();
+
+    /**
+     * @brief getType - Gets the item type of the
+     * particular compost item
+     * @return The ItemType enum: Trash = 0,
+     * Recycle = 1, Compost = 2, Special = 3.
+     */
+    ItemType getType();
+
+    /**
+     * @brief getName - Gets the specific name of
+     * the item
+     * @return String representation of the item's name
+     */
+    std::string getName();
+
+    /**
+     * @brief getDescription - Gets the specific
+     * description of the item
+     * @return String representation of the item's
+     * description.
+     */
+    std::string getDescription();
+private:
+    /**
+     * @brief name - The specific item's name
+     */
+    std::string name;
+
+    /**
+     * @brief description - The specific item's
+     * description.
+     */
+    std::string description;
+
+    /**
+     * @brief itemImage - The specific item's
+     * icon
+     */
+    QImage itemImage;
+
+    /**
+     * @brief setImage - A helper method that
+     * sets the specific image for the item
+     * @param index - Indicates which item icon to set
+     */
+    void setImage(int index);
+
+    /**
+     * @brief setDescription - A helper method that
+     * sets the specific description for the item
+     * @param index - Indicates which item description
+     */
+    void setDescription(int index);
+
+    /**
+     * @brief setName - A helper method that set the
+     * specific name for the item
+     * @param index - Indicates which item name
+     */
+    void setName(int index);
 };
 
 #endif // COMPOSTITEMS_H
