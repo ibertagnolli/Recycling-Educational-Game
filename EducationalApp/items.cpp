e586bc04f9a9c62bc7a735b1e5fc1bb3506152e9
VistaMarston <63620520+VistaMarston@users.noreply.github.com>
2022-04-16 13:08:26 -0600

    Updated the Recycle Item classes
    Created method and implemented methods that are used for the creation of the recycle can class.

--- EducationalApp/items.cpp
+++ EducationalApp/items.cpp
@@ -1,6 +1,2 @@
 #include "items.h"
 
-Items::Items()
-{
-
-}
--- EducationalApp/recycleitems.cpp
+++ EducationalApp/recycleitems.cpp
@@ -1,6 +1,66 @@
 #include "recycleitems.h"
 
-RecycleItems::RecycleItems()
-{
 
+RecycleItems::RecycleItems(int index)
+{
+    setItemImage(index);
+    setItemName(index);
+    setItemDescription(index);
 }
+
+std::string RecycleItems::getName() {
+    return name;
+}
+
+std::string RecycleItems::getDescription(){
+    return description;
+}
+
+Items::ItemType RecycleItems::getType(){
+    return Items::ItemType::Recycle;
+}
+
+void RecycleItems::setItemImage(int index){
+    switch(index){
+    case 0: { // Water Bottle
+        QImage image(":/images/images/pinkTulip1.png");
+        itemImage = image.copy();
+        break;
+    } case 1: { // Cardboard Boxes
+        QImage image(":/images/images/pinkTulip1.png");
+        itemImage = image.copy();
+        break;
+    }
+    }
+}
+
+void RecycleItems::setItemName(int index) {
+    switch(index) {
+    case 0: {
+        name = "Water Bottle";
+        break;
+    }
+    case 1: {
+        name = "Cardboard Boxes";
+    }
+    }
+}
+
+void RecycleItems::setItemDescription(int index){
+    switch(index) {
+    case 0: { // Water Bottle
+        description = "Any plastic bottle, tub, or jug - "
+                      "as long as it is clean and dry - can be"
+                      "recycled in Salt Lake County regardless of the "
+                      "number or chasing arrows";
+        break;
+    }
+    case 1: {
+        description = "Cardboard boxes, shoe boxes, cereal boxes, and paper tubes "
+                      "all can be placed in the curbside recycle bin. Break down the "
+                      "boxes before placing them in the bin.";
+        break;
+    }
+    }
+}
+
